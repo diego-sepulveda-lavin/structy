725a533591d2eb4c082480ef4f2506fdf6d7840a
Diego Sepulveda <diegosl@hotmail.cl>
2021-08-19 20:18:28 -0400

    add 000 problem solution1

--- src/000-hey-programmer.ts
+++ src/000-hey-programmer.ts
@@ -4,7 +4,15 @@
 
     Write a function greet that takes in a string argument, s, and returns the string "hey s". 
     No tricks here. Run the tests to check your work.
+
+    greet("alvin"); // -> 'hey alvin'
 */
 
-const greet = (s: string) => {
+const mystr = "alvin";
+
+const greet = (s: string): string => {
+  return "hey " + s;
 };
+
+console.log(greet(mystr));
+
