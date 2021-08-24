0b961d2afe7d0109ef139238d2159caddabfaf46
Diego Sepulveda <diegosl@hotmail.cl>
2021-08-23 21:30:11 -0400

    add problem 005 solution3

--- src/005-anagrams.ts
+++ src/005-anagrams.ts
@@ -38,9 +38,29 @@ const anagrams2 = (str1: string, str2: string): boolean => {
   str1Arr.sort();
   str2Arr.sort();
 
-
   for (let i = 0; i < str1Arr.length; i++) {
     if (str1Arr[i] !== str2Arr[i]) return false;
+  }
+  return true;
+};
+
+const anagrams3 = (str1: string, str2: string): boolean => {
+  if (str1.length !== str2.length) return false;
+
+  let str1Map = new Map();
+  let str2Map = new Map();
+
+  for (const char of str1) {
+    !str1Map.has(char) ? str1Map.set(char, 1) : str1Map.set(char, str1Map.get(char) + 1);
+  }
+
+  for (const char of str2) {
+    !str2Map.has(char) ? str2Map.set(char, 1) : str2Map.set(char, str2Map.get(char) + 1);
+  }
+
+  for (const char of str1Map) {
+    if (!str2Map.has(char[0])) return false;
+    if (str1Map.get(char[0]) !== str2Map.get(char[0])) return false;
   }
   return true;
 };
@@ -51,4 +71,9 @@ console.log(anagrams("cats", "tocs")); // false
 
 // O(n*log(n)) Time | O(n) Space
 console.log(anagrams2("restful", "fluster")); // true
-console.log(anagrams2("cats", "tocs")); // false
+console.log(anagrams2("cats", "tocs")); // false */
+
+// O(n) Time | O(n) Space
+console.log(anagrams3("restful", "fluster")); // true
+console.log(anagrams3("cats", "tocs")); // false
+console.log(anagrams3("paper", "reapa")); // -> false
