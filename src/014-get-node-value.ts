2fe0dfe870b66bc811dcb3f122f346fd7b62c37b
Diego Sepulveda <diegosl@hotmail.cl>
2021-08-26 15:08:23 -0400

    add problem 014 solution2

--- src/014-get-node-value.ts
+++ src/014-get-node-value.ts
@@ -59,5 +59,16 @@ const getNodeValue = (node: Node, index: number) => {
   return null;
 };
 
+const getNodeValue2 = (node: Node | null, searchedIdx: number, currIdx: number = 0): string | null => {
+  if (node === null) return null;
+  if (searchedIdx === currIdx) return node.value;
+  return getNodeValue2(node.next, searchedIdx, currIdx + 1);
+};
+
+// O(n) Time | O(1) Space
 // a -> b -> c -> d
 console.log(getNodeValue(a, 2)); // 'c'
+
+// O(n) Time | O(n) Space
+// a -> b -> c -> d
+console.log(getNodeValue2(a, 2)); // 'c'
